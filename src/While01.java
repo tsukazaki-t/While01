e5df11b17754bcac1a7d7faf6eadf008eb896b74
TomomiTsukazaki <122886012+tsukazaki-t@users.noreply.github.com>
2023-02-12 22:05:05 +0900

    課題提出

---
+++ src/While01.java
@@ -0,0 +1,16 @@
+
+public class While01 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		int number = 1; 
+		
+		while(number < 50) {
+			number *= 2; 
+			System.out.println("While01 = " + number);
+		}
+
+	}
+
+}
